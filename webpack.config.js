95c5f4352f9cfb4b58fa72b7a3d55f2f2a0a4ecd
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-01-28 13:32:14 +0100

    Determine __APP_NAME__ from appinfo/info.xml

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,33 @@ const webpack = require('webpack');
 const xmlReader = require('xml-reader');
 const fs = require('fs');
 
-
+function appName() {
+  const infoFile = path.join(__dirname, 'appinfo/info.xml');
+  const xmlData = fs.readFileSync(infoFile);
+  const result = xmlReader.parseSync(xmlData.toString());
+  for (const child of result.children) {
+    if (child.name === 'id') {
+      return child.children[0].value;
+    }
+  }
+  throw new Error('App-Name not found in ' + infoFile);
+}
 
 module.exports = {
   entry: {
     app: './src/index.js',
     refresh: './src/refresh.js',
-    'admin-settings': './src/admin-settings.js'
+    'admin-settings': './src/admin-settings.js',
+    popup: './src/popup.js'
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'js'),
   },
-  devtool: false,//'source-map',
+  devtool: false, // 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      __APP_NAME__: JSON.stringify('dokuwikiembedded'),
+      __APP_NAME__: JSON.stringify(JSON.stringify(appName()))
     }),
   ],
 };

a01e974459858c55ac59cff37af3b3678377f2a3
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-01-25 14:36:12 +0100

    Rename DokuWikiEmbedded -> TextDokuWiki.

--- lib/Settings/Admin.php
+++ lib/Settings/Admin.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * DokuWikiEmbedded -- Embed DokuWiki into NextCloud with SSO.
+ * TextDokuWiki -- Embed DokuWiki into NextCloud with SSO.
  *
  * @author Claus-Justus Heine <himself@claus-justus-heine.de>
  * @copyright 2020, 2021, 2022, 2023 Claus-Justus Heine
  * @license AGPL-3.0-or-later
  *
- * DokuWikiEmbedded is free software: you can redistribute it and/or
+ * TextDokuWiki is free software: you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
- * DokuWikiEmbedded is distributed in the hope that it will be useful,
+ * TextDokuWiki is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * AFFERO GENERAL PUBLIC LICENSE for more details.
  *
  * You should have received a copy of the GNU Affero General Public
- * License along with DokuWikiEmbedded. If not, see
+ * License along with TextDokuWiki. If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
-namespace OCA\DokuWikiEmbedded\Settings;
+namespace OCA\TextDokuWiki\Settings;
 
 use OCP\AppFramework\Http\TemplateResponse;
 use OCP\IURLGenerator;

2ebc87640f73bc54edb51d6d29d0d9beee684518
Claus-Justus Heine <himself@claus-justus-heine.de>
2023-07-11 15:10:29 +0200

    Add assets to stable26

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rotdrop/dokuwiki-nextcloud',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ea605d063d14f649f1bf29166df79164321fdb88',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'phpxmlrpc/phpxmlrpc' => array(
+            'pretty_version' => '4.10.1',
+            'version' => '4.10.1.0',
+            'reference' => 'ac18e315ac200692f3b02c773036c24be0acbefa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpxmlrpc/phpxmlrpc',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rotdrop/dokuwiki-nextcloud' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ea605d063d14f649f1bf29166df79164321fdb88',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

eb14fb2632560b8a1fb8a75b001e40aa49c4aff0
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-01-09 16:17:37 +0100

    Add a missing use

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -37,6 +37,8 @@ use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\App;
 
+use OCA\DokuWiki\Listener\Registration as ListenerRegistration;
+
 /*
  *
  **********************************************************

b57e479deae9db373edd582be46f1949392872d3
Claus-Justus Heine <himself@claus-justus-heine.de>
2024-03-13 09:48:30 +0100

    Update assets

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rotdrop/dokuwiki-nextcloud',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5c9f030044e2ec0edf8b70d17d52b7a9ef85ce49',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'phpxmlrpc/phpxmlrpc' => array(
+            'pretty_version' => '4.10.1',
+            'version' => '4.10.1.0',
+            'reference' => 'ac18e315ac200692f3b02c773036c24be0acbefa',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpxmlrpc/phpxmlrpc',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rotdrop/dokuwiki-nextcloud' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5c9f030044e2ec0edf8b70d17d52b7a9ef85ce49',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

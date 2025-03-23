ed59eb56ca03a9c222170314bdde890e730c8df0
Claus-Justus Heine <himself@claus-justus-heine.de>
2025-03-23 18:27:15 +0100

    Update import after changing to TS

--- src/refresh.ts
+++ src/refresh.ts
@@ -22,7 +22,7 @@
 
 import { getCurrentUser } from '@nextcloud/auth';
 import axios from '@nextcloud/axios';
-import onDocumentLoaded from './toolkit/util/on-document-loaded.js';
+import onDocumentLoaded from './toolkit/util/on-document-loaded.ts';
 import { generateUrl } from './toolkit/util/generate-url.js';
 import getInitialState from './toolkit/util/initial-state.ts';
 import type { InitialState } from './types/initial-state.d.ts';
